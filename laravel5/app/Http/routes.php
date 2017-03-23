506e1e38276f3a83cd50d28b3f94ab8a24a2379b
DennyAdiSiswanto <dennyadisiswanto2.das@gmail.com>
2017-03-23 23:20:21 +0800

    Post Test 4

---
+++ laravel5/app/Http/routes.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('pengguna', 'penggunaController@awal');
+Route::get('pengguna/tambah', 'penggunaController@tambah');
+Route::get('pengguna/{pengguna}','PenggunaController@lihat');
+Route::post('pengguna/simpan','PenggunaController@simpan@simpan');
+Route::get('pngguna/edit/{pengguna}','PenggunaController@edit');
+Route::post('pngguna/edit/{pengguna}','PenggunaController@update');
+Route::get('pngguna/hapus/{pengguna}','PenggunaController@hapus');
+
+Route::get('dosen','Dosencontroller@awal');
+Route::get('dosen/tambah','Dosencontroller@tambah');
+
+Route::get('mahasiswa','mahasiswacontroller@awal');
+Route::get('mahasiswa/tambah','mahasiswacontroller@tambah');
+
+Route::get('matakuliah','matakuliahcontroller@awal');
+Route::get('matakuliah/tambah','matakuliahcontroller@tambah');
+
+Route::get('jadwal_matakuliah','jadwal_matakuliahcontroller@awal');
+Route::get('jadwal_matakuliah/tambah','jadwal_matakuliahcontroller@tambah');
+
+Route::get('ruangan','ruangancontroller@awal');
+Route::get('ruangan/tambah','ruangancontroller@tambah');
+
+Route::get('dosen_matakuliah','dosen_matakuliahcontroller@awal');
+Route::get('dosen_matakuliah/tambah','dosen_matakuliahcontroller@tambah');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/public', function () {
+    return view('nofal');
+});
+
+
